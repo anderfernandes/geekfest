2653c7da38aa4faea584f1a0ded67eeaedcd9116
anderfernandes <handerson171@gmail.com>
2017-05-01 15:04:38 -0500

    Heroku pgsql setup

--- database/database.php
+++
@@ -1,9 +0,0 @@
-'sqlsrv' => [
-    'driver' => 'sqlsrv',
-    'host' => env('DB_HOST', 'geekfest.database.windows.net'),
-    'database' => env('DB_DATABASE', 'geekfest'),
-    'username' => env('DB_USERNAME', 'geekfest'),
-    'password' => env('DB_PASSWORD', 'Paracuru2'),
-    'charset' => 'utf8',
-    'prefix' => '',
-],
